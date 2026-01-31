b8f1b1c05041cf11e3486f8ebcb8cb9b134e75b2
MrFaultyT <wanye787@gmail.com>
2026-01-31 09:42:11 +0000

    Initialize pokedexcli with Hello World

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Wayne-Francis/pokedexcli
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// main function: program execution starts here
+func main() {
+    fmt.Println("Hello, World!")
+}
