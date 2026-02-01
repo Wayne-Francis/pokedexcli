3d4d79d35b62664340cedd494c17aed47e6d0490
MrFaultyT <wanye787@gmail.com>
2026-02-01 15:29:36 +0000

    implemented the bones of the REPL into the main.go

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+    "os"
+)
 
 // main function: program execution starts here
 func main() {
-    fmt.Println("Hello, World!")
+    scanner := bufio.NewScanner(os.Stdin)
+    for {
+        fmt.Print("Pokedex > ")
+        scanner.Scan()         // moves to the next line
+		line := scanner.Text()     // gets the current line
+        clean_input := cleanInput(line)
+        first_word := clean_input[0]
+        fmt.Printf("Your command was: %s\n", first_word)
+    }
 }
