14ebef7d51b76f9fa09cf9aa2f0d8574121d8d54
MrFaultyT <wanye787@gmail.com>
2026-02-02 10:27:21 +0000

    a few updates added here, added a registry of commands, and registered 2 commands, help and exit

--- main.go
+++ main.go
@@ -10,11 +10,19 @@ import (
 func main() {
     scanner := bufio.NewScanner(os.Stdin)
     for {
-        fmt.Print("Pokedex > ")
+        fmt.Println("Pokedex > ")
         scanner.Scan()         // moves to the next line
 		line := scanner.Text()     // gets the current line
         clean_input := cleanInput(line)
-        first_word := clean_input[0]
-        fmt.Printf("Your command was: %s\n", first_word)
+        input := clean_input[0]
+        cmd, ok := commands[input]
+        if !ok {
+            fmt.Printf("Unknown command: %v\n", input)
+            continue
+        }
+        err := cmd.callback()
+        if err != nil {
+            fmt.Println(err)
+        }
     }
 }
